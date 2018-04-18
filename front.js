ee39646f0cb67b78d65505d6dd44e6bb90191ba8
tj Bains <tjbains1943@yahoo.com>
2018-04-17 20:20:08 -0700

    add Constructor

--- front.js
+++ front.js
@@ -1,8 +1,17 @@
 
 
 console.log("penis");
+function Group(name, phoneNumber, email, unique) {
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.unique = unique;
+}
+
 
 $("#tableRes").on("click", function(res) {
 res.preventDefault();
+var name = $()
+var newCust = Group()
 console.log("penis");
 })
